c9daf3bf29cbeb60ff9a81ba75396e460a01d469
Jason Etcovitch <jasonetco@github.com>
2020-06-17 19:19:36 -0400

    removeBranchProtection should use default branch

--- actions/removeBranchProtection/index.js
+++ actions/removeBranchProtection/index.js
@@ -1,5 +1,7 @@
+const getDefaultBranch = require('../../helpers/get-default-branch')
+
 module.exports = async (context, opts) => {
   return context.github.repos.removeBranchProtection(context.repo({
-    branch: opts.branch || 'master'
+    branch: opts.branch || getDefaultBranch(context)
   }))
 }
--- actions/removeBranchProtection/schema.js
+++ actions/removeBranchProtection/schema.js
@@ -3,8 +3,7 @@ const Joi = require('@hapi/joi')
 module.exports = Joi.object({
   branch: Joi.string()
     .meta({ label: 'Branch' })
-    .description('The name of the branch to remove protection from. This defaults to `master`.')
-    .default('master')
+    .description('The name of the branch to remove protection from. This will be the repo\'s default branch if none was provided.')
 })
   .description('Removes the branch protection on a branch in the course repository')
   .example([
